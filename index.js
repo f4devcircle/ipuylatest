e827267c853e097ffcdaa167907fcbb0f8a49eb7
Faris Widyantho <faris.widyantho@orori.com>
2018-10-07 15:16:36 +0700

    fixed

--- index.js
+++ index.js
@@ -1,49 +1,38 @@
-'use strict';
+const line = require('@line/bot-sdk')
+const app = require('express')()
+const PORT = process.env.PORT || 5000
 
-const line = require('@line/bot-sdk');
-const express = require('express');
-
-// create LINE SDK config from env variables
 const config = {
   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
-  channelSecret: process.env.CHANNEL_SECRET,
-};
+  channelSecret: process.env.CHANNEL_SECRET
+}
+const client = new line.Client(config)
 
-// create LINE SDK client
-const client = new line.Client(config);
+app.get('/', (req, res) => {
+  res.send({ message: 'hello world' })
+})
 
-// create Express app
-// about Express itself: https://expressjs.com/
-const app = express();
-
-// register a webhook handler with middleware
-// about the middleware, please refer to doc
 app.post('/callback', line.middleware(config), (req, res) => {
-  Promise
-    .all(req.body.events.map(handleEvent))
-    .then((result) => res.json(result))
-    .catch((err) => {
-      console.error(err);
-      res.status(500).end();
-    });
-});
+  Promise.all(req.body.events.map(handleEvent))
+    .then(result => {
+      res.status(200).send(result)
+    })
+    .catch(err => {
+      console.log('error:', err)
+      res.status(500).end()
+    })
+})
 
-// event handler
-function handleEvent(event) {
+const handleEvent = (event) => {
   if (event.type !== 'message' || event.message.type !== 'text') {
-    // ignore non-text-message event
-    return Promise.resolve(null);
+    return Promise.resolve(null)
+  }
+  const echo = {
+    type: 'text',
+    text: event.message.text
   }
 
-  // create a echoing text message
-  const echo = { type: 'text', text: event.message.text };
-
-  // use reply API
-  return client.replyMessage(event.replyToken, echo);
+  return client.replyMessage(event.replyToken, echo)
 }
 
-// listen on port
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`listening on ${port}`);
-});
+app.listen(PORT, () => console.log(`listening ${PORT}`))
